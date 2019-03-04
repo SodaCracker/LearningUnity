311376eb61ead39451d06aff5cb2168520a8bf9b
Wei Yue <genius0610@gmail.com>
2019-03-04 22:57:09 +0800

    Update ScriptableObjectVariables.cs

--- Game Architecture/ScriptableObjectVariables.cs
+++ Game Architecture/ScriptableObjectVariables.cs
@@ -1,6 +1,39 @@
 [CreateAssetMenu]
-
 public class FloatVariable : SriptableObject
 {
+    public float Value;
+}
 
+// Hitpoints
+// Damage Amounts
+// Timing Data
+
+/*
+public class DumbEnemy : MonoBehaviour
+{
+    public FloatVariable MaxHP;
+    public FloatVariable MoveSpeed;
+}
+*/
+
+
+
+// Float Reference
+[Serializable]
+public class FloatReference
+{
+    public bool UseConstant = true;
+    public float ConstantValue;
+    public FloatVariable Variable;
+  
+    public float Value
+    {
+        get {  return UseConstant ? ConstantValue : Variable.Value}
+    }
+}
+
+public class DumbEnemy : MonoBehaviour
+{
+    public FloatReference MaxHP;
+    public FloatReference MoveSpeed;
 }
